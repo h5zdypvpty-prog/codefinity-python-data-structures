d7208c52a2977d1237c45209446bbe90b346a908
h5zdypvpty-prog <>
2026-02-13 18:02:06 +0000

    List insertion

---
+++ section/list_insertion/main.py
@@ -0,0 +1,8 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney"]
+
+# Adding new elements
+travel_wishlist.insert('London', 'Budapest')
+
+
+# Testing 
+print("Updated travel_wishlist:", travel_wishlist)
